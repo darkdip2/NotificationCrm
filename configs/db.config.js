0f1de6af259b81011a35308464e4996f7cd6cd4c
dark_dip2 <diptya99@gmail.com>
2023-06-27 01:21:28 +0530

    dbconfig

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,6 +1,6 @@
 module.exports=
 {
     DB_NAME:'notification_db',
-    DB_URL:'mongodb+srv://diptya99:<password>@cluster0.z4i3cww.mongodb.net/'
+    DB_URL:'mongodb+srv://diptya99:Password67@cluster0.z4i3cww.mongodb.net/'
     //'mongodb://localhost/notification_db'
 }
